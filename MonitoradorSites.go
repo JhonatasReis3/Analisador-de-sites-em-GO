b825557aa41cfd6db815c39e6c5ffa9cd4a32c67
JhonatasReis3 <programador.jhonatasreis@gmail.com>
2025-02-19 16:48:36 -0300

    refotaração de erro nas chamadas das funções

--- MonitoradorSites.go
+++ MonitoradorSites.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"net/http"
 	"os"
 	"strings"
@@ -13,17 +12,19 @@ func main() {
 	apresentacao()
 }
 func apresentacao() {
-	fmt.Println("Bem vindo escolha uma opção")
-	menu()
+	fmt.Println("---Bem vindo escolha uma opção---")
+	opcao := menuBoasvindas()
+	fmt.Println(opcao)
 }
-func menu() int {
+func menuBoasvindas() int {
 	opcoes := []string{
-		"1- Listar sites",
-		"2- Iniciar Monitoramento",
-		"3- Exibir logs",
-		"4- Sair do programa",
+		"1-Listar sites",
+		"2-Iniciar Monitoramento",
+		"3-Exibir logs",
+		"4-Sair do programa",
 	}
 	var opcao int
+	sitesCadastrados := ObterSites()
 	for {
 		for i := 0; i < len(opcoes); i++ {
 			fmt.Println(opcoes[i])
@@ -33,59 +34,53 @@ func menu() int {
 			break
 		}
 	}
-	escolha(opcao)
+	escolha(opcao, sitesCadastrados)
 	return opcao
+	/*
+		o erro está que a variavel opcao está compilando o numero do comando, preciso resolver
+	*/
 }
 
-func escolha(opcao int) {
+func escolha(opcao int, sitesCadastrados []string) {
 	switch opcao {
 	case 1:
-		ListarSites()
+		ListarSites(sitesCadastrados)
 	case 2:
-		monitoramento()
+		monitoramento(sitesCadastrados)
 	case 3:
 		println("Exibidindologs")
 	case 4:
 		fmt.Println("Saindo do programa")
 		os.Exit(0)
 	}
-
 }
-func ListarSites() {
+func ObterSites() []string {
 	arquivo, _ := os.Open("sites.txt")
-	var sites []string
+	defer arquivo.Close()
+	var sitesCadastrados []string
+	scanner := bufio.NewScanner(arquivo)
+	for scanner.Scan() {
+		site := strings.TrimSpace(scanner.Text())
+		if site != "" {
+			sitesCadastrados = append(sitesCadastrados, site)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return sitesCadastrados
+	}
+
+	return sitesCadastrados
+}
+func ListarSites(sitesCadastrados []string) {
 	fmt.Println("---Lista de Sites Cadastrados---")
-	fmt.Println("==================================")
-	for {
-		lerSites := bufio.NewReader(arquivo)
-		site, erro := lerSites.ReadString('\n')
-		site = strings.TrimSpace(site)
-		sites = append(sites, site)
-		for index, site := range sites {
-			fmt.Println(index, "-", site)
-		}
-		if erro == io.EOF {
-			break
-		}
+	for index, site := range sitesCadastrados {
+		fmt.Println(index, "-", site)
 	}
-	fmt.Println("==================================")
+	fmt.Println("=======================")
 }
-func monitoramento() []string {
-	fmt.Println("Digite quantos sites voce quer monitora *apenas numeros")
-	var numero_sites int
-	fmt.Scan(&numero_sites)
-	sites := []string{}
-	for i := 0; i < numero_sites; i++ {
-		fmt.Printf("digite o site")
-		var site string
-		fmt.Scan(&site)
-		sites = append(sites, site)
-	}
-	testando_sites(sites)
-	reiniciar()
-	return sites
-}
-func testando_sites(sites []string) {
+func monitoramento(sitesCadastrados []string) []string {
+	sites := sitesCadastrados
 	for j := 0; j < len(sites); j++ {
 		resposta, _ := http.Get(sites[j])
 		if resposta.StatusCode == 200 {
@@ -94,27 +89,5 @@ func testando_sites(sites []string) {
 			fmt.Println("o site não tá funcionado", sites[j])
 		}
 	}
+	return sites
 }
-func reiniciar() string {
-	var reiniciar string
-	fmt.Println("Deseja fazer uma nova consulta?")
-	fmt.Println("1- SIM")
-	fmt.Println("2- NÃO")
-	fmt.Scan(&reiniciar)
-	resposta := strings.ToUpper(reiniciar)
-	if resposta == "SIM" || resposta == "1" {
-		monitoramento()
-	} else if resposta == "NÃO" || resposta == "2" {
-		menu()
-	} else {
-
-	}
-	return resposta
-}
-
-/* Criações pendentes
--criar o arquivo txt
--fazer um menu interativo que cadastre,edite,ou esclua sites
--programar consultas
--enviar via emails erros
-*/
